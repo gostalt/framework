52cbcbb971ad0456dc63cfc79632842a28b91a46
Tom Smith <me@tomm.us>
2019-08-15 00:10:59 +0100

    move makers

---
+++ maker/handler.go
@@ -0,0 +1,60 @@
+package maker
+
+import (
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+const handlerStub = `package <package>
+
+import (
+	"net/http"
+)
+
+func <handler>(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}`
+
+type HandlerMaker struct {
+	Path string
+}
+
+func (m HandlerMaker) makeDirPath(handler string) string {
+	rootDir := strings.Split(m.Path, "/")
+	handlerDirPieces := strings.Split(handler, ".")
+
+	return filepath.Join(
+		append(rootDir, handlerDirPieces[:len(handlerDirPieces)-1]...)...,
+	)
+}
+
+// Make creates a new file with the contents of the stub.
+func (m HandlerMaker) Make(name string) error {
+
+	dir := m.makeDirPath(name)
+
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		return err
+	}
+
+	handlerPieces := strings.Split(name, ".")
+	pkg := handlerPieces[len(handlerPieces)-2]
+	file := strings.Title(strings.ToLower(handlerPieces[len(handlerPieces)-1]))
+
+	path := dir + "/" + file + ".go"
+
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	content := strings.Replace(handlerStub, "<package>", pkg, -1)
+	content = strings.Replace(content, "<handler>", file, -1)
+
+	if _, err := f.Write([]byte(content)); err != nil {
+		return err
+	}
+
+	return nil
+}
