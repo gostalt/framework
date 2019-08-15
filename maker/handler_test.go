3ddff48a958db56a1851b866e0d47b363f63a8b2
Tomy Smith <me@tomm.us>
2019-08-15 15:13:32 +0100

    add path test to handler maker

---
+++ maker/handler_test.go
@@ -0,0 +1,18 @@
+package maker
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestMakePath(t *testing.T) {
+	m := HandlerMaker{Path: "a/long/path/"}
+
+	path := m.makeDirPath("nested.to.handler")
+	expected := "a/long/path/nested/to"
+
+	if path != expected {
+		fmt.Printf("Got  %s\nWant %s\n", path, expected)
+		t.FailNow()
+	}
+}
