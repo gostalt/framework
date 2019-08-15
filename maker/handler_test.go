88ad4c020454447c88ce0ebe0fa94c86d3a093a4
Tomy Smith <me@tomm.us>
2019-08-15 15:45:01 +0100

    move write to separate method

--- maker/handler_test.go
+++ maker/handler_test.go
@@ -1,6 +1,7 @@
 package maker
 
 import (
+	"bytes"
 	"fmt"
 	"testing"
 )
@@ -16,3 +17,28 @@ func TestMakeDirPath(t *testing.T) {
 		t.FailNow()
 	}
 }
+
+func TestWriteContents(t *testing.T) {
+	var b bytes.Buffer
+	m := HandlerMaker{Path: "/"}
+
+	if err := m.writeContent(&b, "example", "ExampleHandler"); err != nil {
+		t.Fatalf("Got error from writeContent: %s", err)
+	}
+
+	got := b.String()
+	want := handlerTestStub
+	if got != want {
+		t.Errorf("Got  %s\nWant %s\n", got, want)
+	}
+}
+
+const handlerTestStub = `package example
+
+import (
+	"net/http"
+)
+
+func ExampleHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World!"))
+}`
