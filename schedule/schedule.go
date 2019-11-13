79ca2d28fc85224862aeaf9004bddb4411c074a1
Tom Smith <me@tomm.us>
2019-11-13 20:32:05 +0000

    allow schedule to be ran concurrently

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -1,5 +1,7 @@
 package schedule
 
+import "sync"
+
 // Job represents a piece of work that should be carried out when a
 // certain criteria, determined by the `ShouldFire` method, is met.
 type Job interface {
@@ -15,18 +17,34 @@ type Runner struct {
 	jobs []Job
 }
 
-// Run iterates through the Jobs container in the Runner and
-// determines if they should be ran. If so, it calls the Handle
-// method on the Job.
-func (s *Runner) Run() {
-	for _, job := range s.jobs {
-		if job.ShouldFire() {
-			job.Handle()
-		}
+// NewRunner creates a new runner from a list of Jobs.
+func NewRunner(jobs ...Job) Runner {
+	return Runner{
+		jobs: jobs,
 	}
 }
 
+// Run iterates through the Jobs container in the Runner and
+// determines if they should be ran. If so, it calls the Handle
+// method on the Job.
+func (r Runner) Run() {
+	wg := sync.WaitGroup{}
+	for _, job := range r.jobs {
+		if job.ShouldFire() {
+			wg.Add(1)
+
+			// https://github.com/golang/go/wiki/CommonMistakes#using-goroutines-on-loop-iterator-variables
+			go func(job Job) {
+				job.Handle()
+				wg.Done()
+			}(job)
+		}
+	}
+
+	wg.Wait()
+}
+
 // Add adds another Job to the Runner.
-func (s *Runner) Add(jobs ...Job) {
-	s.jobs = append(s.jobs, jobs...)
+func (r Runner) Add(jobs ...Job) {
+	r.jobs = append(r.jobs, jobs...)
 }
--- schedule/schedule_test.go
+++ schedule/schedule_test.go
@@ -1,11 +1,12 @@
 package schedule
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
 
 func TestJobsCanBeAddedToRunner(t *testing.T) {
-	r := Runner{}
-
-	r.Add(&TestJob{})
+	r := NewRunner(&TestJob{})
 
 	if len(r.jobs) != 1 {
 		t.Errorf("expected 1 job, got %d", len(r.jobs))
@@ -13,24 +14,33 @@ func TestJobsCanBeAddedToRunner(t *testing.T) {
 }
 
 func TestRunnerRunsJobs(t *testing.T) {
-	r := Runner{}
-
 	j := &TestJob{}
 
-	r.Add(j)
-
-	r.Run()
+	NewRunner(j).Run()
 
 	if !j.hasFired {
 		t.Error("TestJob did not fire")
 	}
 }
 
+// This test doesn't really test anything programatically. It simply
+// runs the Handler for each of the TestJobs, which, when ran several
+// times, would display the concurrency ability.
+func TestJobsRunConcurrently(t *testing.T) {
+	j1 := &TestJob{Input: "asd"}
+	j2 := &TestJob{Input: "123"}
+	j3 := &TestJob{Input: "zxc"}
+
+	NewRunner(j1, j2, j3).Run()
+}
+
 type TestJob struct {
+	Input    string
 	hasFired bool
 }
 
 func (t *TestJob) Handle() error {
+	log.Println(t.Input)
 	t.hasFired = true
 	return nil
 }
