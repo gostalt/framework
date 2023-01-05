0685511e106f56a7430ef4c1fc69e2f7fede7c38
Thomas Smith <me@tomm.us>
2023-01-05 10:08:56 +0000

    Add error return value
    Refs #9

--- service/provider.go
+++ service/provider.go
@@ -2,21 +2,24 @@ package service
 
 import "github.com/sarulabs/di/v2"
 
-// Provider is a piece of the app that provides a service for the
-// developer, such as a database or logger. Providers can Register
-// items into the DI Container, and can perform actions.
+// Providers are a feature of Gostalt that bootstraps the entire application. Each
+// component, such as routing and database interaction, is governed by its own
+// service provider.
 type Provider interface {
-	Register(*di.Builder)
-	Boot(di.Container)
+	Register(*di.Builder) error
+	Boot(di.Container) error
 }
 
-// BaseProvider provides a blank canvas to ensure that Providers
-// satisfy the Provider interface.
+// BaseProvider enables developers to use embedding to ensure that Providers that
+// they have created themselves satisfy the Provider interface.
 type BaseProvider struct{}
 
-// Register is called on all Providers in sequence when the app
-// is first loaded.
-func (p BaseProvider) Register(b *di.Builder) {}
+// Register is called on all Providers in sequence when the app is first loaded.
+func (p BaseProvider) Register(b *di.Builder) error {
+	return nil
+}
 
 // Boot is called when all the Providers have been Registered.
-func (p BaseProvider) Boot(c di.Container) {}
+func (p BaseProvider) Boot(c di.Container) error {
+	return nil
+}
